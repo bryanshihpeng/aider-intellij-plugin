a02a37323233075c115049ce169dda2b428b8f8e
GitHub Action <action@github.com>
2023-10-31 10:34:00 +0000

    Template cleanup

--- src/main/kotlin/com/github/bryanshihpeng/aiderintellijplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/bryanshihpeng/aiderintellijplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.bryanshihpeng.aiderintellijplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.bryanshihpeng.aiderintellijplugin.MyBundle
+import com.github.bryanshihpeng.aiderintellijplugin.services.MyProjectService
 import javax.swing.JButton
 
 
